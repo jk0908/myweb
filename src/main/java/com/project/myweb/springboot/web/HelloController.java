4e8aa681c424c0c1e8036c251ab5b76a5a163f19
jk0908 <biblebest@naver.com>
2020-06-04 23:11:26 +0900

    HelloController 생성 및 코드 작성

--- src/main/java/com/project/myweb/springboot/web/HelloController.java
+++ src/main/java/com/project/myweb/springboot/web/HelloController.java
@@ -1,4 +1,13 @@
 package com.project.myweb.springboot.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
